b4d0027a2fb9a6aa30f26ed97563a9e3bf40a5a8
rasujon3@gmail.com <rasujon3@gmail.com>
2024-01-20 21:57:32 +0600

    cart list show, add, remove done.

--- resources/views/pages/cart-list-page.blade.php
+++ resources/views/pages/cart-list-page.blade.php
@@ -7,8 +7,10 @@
     @include('component.Footer')
     <script>
         (async () => {
+            await Category();
             await CartList();
             $(".preloader").delay(90).fadeOut(100).addClass('loaded');
+            await TopBrands();
         })()
     </script>
 @endsection
